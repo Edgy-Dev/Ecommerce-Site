e43940e2b2430f3b16c331556c98719ba91f77c9
Justin Cook <justin.cook.dev@gmail.com>
2019-01-08 16:38:05 -0800

    Successfully testing the order model

--- server/db/models/order.spec.js
+++ server/db/models/order.spec.js
@@ -1,6 +1,71 @@
-const {expect} = require('chai');
-const db = require('../index');
-const Order = db.model('Order1');
+const {expect} = require('chai')
+const db = require('../index')
+const Order = db.model('Order')
 
-// no tests yet. no apparent model methods needed yet.
+describe('Order model', () => {
+  let order
+  const sequelizeErrors = [
+    'SequelizeValidationError',
+    'SequelizeUniqueConstraintError'
+  ]
 
+  beforeEach(async () => {
+    await db.sync({force: true})
+  })
+
+  beforeEach(() => {
+    order = new Order({
+      card: [{key: 'value'}],
+      userInfo: {key: 'value'},
+      total: 1
+    })
+  })
+
+  it('Should only accept an array of objects for cart', () => {
+    const invalidCards = [['ha'], null, undefined, 'hello', 9, [[]]]
+
+    return Promise.all(
+      invalidCards.map(async invalidItem => {
+        order.card = invalidItem
+        try {
+          await order.validate()
+          throw new Error('This should not have passed')
+        } catch (error) {
+          expect(sequelizeErrors).to.contain(error.name)
+        }
+      })
+    )
+  })
+
+  it('Should only accept JSON for user info', () => {
+    const invalidInformation = [{key: 'value'}, 9, 'hello', []]
+
+    return Promise.all(
+      invalidInformation.map(async invalidItem => {
+        order.userInfo = invalidItem
+        try {
+          await order.validate()
+          throw new Error('This should not have passed')
+        } catch (error) {
+          expect(sequelizeErrors).to.contain(error.name)
+        }
+      })
+    )
+  })
+
+  it('Should only accept integers for total', () => {
+    const invalidTotals = ['hello', {}, null, undefined, undefined]
+
+    return Promise.all(
+      invalidTotals.map(async invalidItem => {
+        order.total = invalidItem
+        try {
+          await order.validate()
+          throw new Error('This should not have passed')
+        } catch (error) {
+          expect(sequelizeErrors).to.contain(error.name)
+        }
+      })
+    )
+  })
+})
