8e371afa22c1d21bb712eebe12576bf46f30da82
jrodger312 <jrodger312@gmail.com>
2019-01-10 10:45:06 -0800

    fixed productreducer and api route

--- client/store/index.js
+++ client/store/index.js
@@ -3,8 +3,9 @@ import createLogger from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
+import productAbstract from './productAbstract'
 
-const reducer = combineReducers({user})
+const reducer = combineReducers({user, products: productAbstract})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
